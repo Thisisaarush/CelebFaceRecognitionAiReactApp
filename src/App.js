1a7b9498d1950c0da0c1af9f63144961c033f9b2
Aarush Tanwar <tanwaraarush007@gmail.com>
2021-07-19 22:26:25 +0530

    removing signin restriction for easier testing

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends React.Component  {
     if (this.state.input) {
       this.setState({imageUrl: this.state.input});
       
-      fetch('https://pure-gorge-23737.herokuapp.com/imageUrl', {
+      fetch('https://localhost:3000/imageUrl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify
@@ -60,7 +60,7 @@ class App extends React.Component  {
         .then(response => response.json())
         .then (response => {
           if (response) {
-            fetch('https://pure-gorge-23737.herokuapp.com/image', {
+            fetch('https://localhost:3000/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify
